57291cbd31767b65562836589b41449e74ea6e4c
Chris Fidao <fideloper@gmail.com>
2013-02-25 19:10:03 -0800

    tested exended model and collections

--- app/tests/Api/Resource/Eloquent/ModelTest.php
+++ app/tests/Api/Resource/Eloquent/ModelTest.php
@@ -3,6 +3,12 @@
 use Mockery as m;
 use Api\Resource\Eloquent\Model;
 
+/**
+* Test protected method?
+*
+* @link http://stackoverflow.com/questions/249664/best-practices-to-test-protected-methods-with-phpunit
+*/
+
 class ModelTest extends TestCase {
 
 	public function tearDown()
@@ -10,19 +16,14 @@ class ModelTest extends TestCase {
 		m::close();
 	}
 
-	public function testGetEtagWhenNotYetGenerated()
+	public function testGetEtag()
 	{
-		$this->assertTrue(true);
-	}
+        $this->_mockConnectionAndStuff();
 
-    public function testGetEtagWhenAlreadyGenerated()
-    {
-        $this->assertTrue(true);
-    }
+		$model = new Model;
 
-    public function testGenerateEtagWithTimestamp()
-    {
-        $model = new Model;
+        // Of course you do...
+        $model->exists = true;
 
         // Timestamps on in this case
         $model->timestamps = true;
@@ -39,22 +40,13 @@ class ModelTest extends TestCase {
         $id = $this->_randString(2);
         $model->setAttribute($model->getKeyName(), $id);
 
-        // Need to mock connection revolver in order to get date format, which is pulled from DB connection !!
-        // "fromDateTime" is the issue
-        // $this->assertTrue( md5($table . $id . $model->fromDateTime($datetime)), $model->generateEtag() );
-        $this->assertTrue(true);
-    }
+        // Get eTag when not generated
+        $expectedEtag = md5($table . $id . $datetime->format('Y-m-d H:i:s'));
+        $this->assertEquals( $expectedEtag , $model->getEtag() );
 
-    public function testGenerateEtagWithoutTimestamp()
-    {
-        $model = new Model;
-
-        $model->timestamps = false;
-
-
-
-        $this->assertTrue(true);
-    }
+        // Get eTag when is generated
+        $this->assertEquals( $expectedEtag, $model->getEtag() );
+	}
 
     public function testCreateNewCollection()
     {
@@ -63,6 +55,15 @@ class ModelTest extends TestCase {
         $collection = $model->newCollection();
 
         $this->assertTrue( $collection instanceof Api\Resource\Eloquent\Collection );
+    }
+
+    protected function _mockConnectionAndStuff()
+    {
+        Illuminate\Database\Eloquent\Model::setConnectionResolver($resolver = m::mock('Illuminate\Database\ConnectionResolverInterface'));
+        $resolver->shouldReceive('connection')->andReturn($mockConnection = m::mock('Illuminate\Database\ConnectionInterface'));
+        $mockConnection->shouldreceive('getPostProcessor')->andReturn(m::mock('Illuminate\Database\Query\Processors\Processor'));
+        $mockConnection->shouldReceive('getQueryGrammar')->andReturn($queryGrammar = m::mock('Illuminate\Database\Query\Grammars\Grammar'));
+        $queryGrammar->shouldReceive('getDateFormat')->andReturn('Y-m-d H:i:s');
     }
 
 
